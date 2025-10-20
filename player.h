ccbedfa3058cb412ff5630dcd363a4c0765b6f3a
inessouibgui04 <inessouibgui04@gmail.com>
2025-10-20 11:04:42 +0200

    Update and rename client.h to player.h

--- player.h
+++ player.h
@@ -1,12 +1,12 @@
-#ifndef CLIENT_H
-#define CLIENT_H
+#ifndef PLAYER_H
+#define PLAYER_H
 
-#include "server2.h"
+#include "server.h"
 
 typedef struct
 {
    SOCKET sock;
    char name[BUF_SIZE];
-}Client;
+}Player;
 
 #endif /* guard */
